7c96bb2afb2c24a4fddd33bf54705c880f4c459b
nhiroyasu <nhiro1109pri@gmail.com>
2024-05-02 19:46:30 +0900

    Implemented navigation to browser extension features

--- wallpaper-play/Page/VideoForm/View/BrowserExtensionViewController.swift
+++ wallpaper-play/Page/VideoForm/View/BrowserExtensionViewController.swift
@@ -1,4 +1,13 @@
 import Cocoa
+import SafariServices
+
+#if DEBUG
+fileprivate let safariExtensionBundleID = "com.debug.nhiro1109.wallpaper-play.safari-extension"
+#else
+fileprivate let safariExtensionBundleID = "com.nhiro1109.wallpaper-play.safari-extension"
+#endif
+
+fileprivate let chromeWebStoreUrl = URL(string: "https://chromewebstore.google.com/detail/wallpaperplay-browser-ext/kdhghbkhfpfoppoaamgbgompifppglme")!
 
 class BrowserExtensionViewController: NSViewController {
 
@@ -7,4 +16,19 @@ class BrowserExtensionViewController: NSViewController {
         // Do view setup here.
     }
     
+    @IBAction func didTapSafariPreferenceButton(_ sender: Any) {
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: safariExtensionBundleID) { error in
+            guard error == nil else {
+                return
+            }
+
+            DispatchQueue.main.async {
+                NSApplication.shared.terminate(nil)
+            }
+        }
+    }
+
+    @IBAction func didTapChromeWebStoreButton(_ sender: Any) {
+        NSWorkspace.shared.open(chromeWebStoreUrl)
+    }
 }
