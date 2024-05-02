5a26dcb3467d7bb34163913fdb6597240fa76a1c
nhiroyasu <nhiro1109pri@gmail.com>
2024-05-02 17:57:09 +0900

    Added Safari extension feature

---
+++ Wallpaper Paly SafariEextension/Resources/background.js
@@ -0,0 +1,59 @@
+const MENU_ID = 'wallpaper-play-for-youtube'
+
+browser.runtime.onInstalled.addListener(function () {
+  browser.contextMenus.create({
+    id: MENU_ID,
+    title: 'Set YouTube video as Wallpaper',
+    contexts: ['all'],
+    documentUrlPatterns: ['*://*.youtube.com/*'], // YouTubeのドメインに限定
+    targetUrlPatterns: ['https://www.youtube.com/.*\bv=[w-]+'],
+  })
+})
+
+browser.contextMenus.onClicked.addListener(function (info, tab) {
+  if (info.menuItemId === MENU_ID) {
+    browser.storage.local.get('userSettings').then((result) => {
+      if (!result.userSettings) {
+        result.userSettings = {
+          isMute: 'false',
+        }
+      }
+
+      const isMute = result.userSettings.isMute
+      if (checkYouTubeUrl(info.linkUrl)) {
+        browser.tabs.create({
+          url: buildUrlScheme(info.linkUrl, isMute),
+        })
+      } else if (checkYouTubeUrl(info.pageUrl)) {
+        browser.tabs.create({
+          url: buildUrlScheme(info.pageUrl, isMute),
+        })
+      } else {
+        browser.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            browser.scripting.executeScript({
+              target: { tabId: tabs[0].id },
+              function: function () {
+                alert('No video ID detected in the URL.')
+              },
+            })
+          }
+        )
+        return
+      }
+    })
+  }
+})
+
+function checkYouTubeUrl(url) {
+  const pattern = /https:\/\/www\.youtube\.com\/.*\bv=[\w-]+/
+  return pattern.test(url)
+}
+
+function buildUrlScheme(youtubeUrl, isMute) {
+  const url = new URL('wallpaperplay://open')
+  url.searchParams.set('youtube-url', youtubeUrl)
+  url.searchParams.set('is-mute', isMute)
+  return url.href
+}
