94bc5303cc9712d64e6ee9c15b9316fc016cc8ae
nhiroyasu <nhiro1109pri@gmail.com>
2025-02-23 22:55:48 +0900

    fix a validation logic for url port number

--- wallpaper-play/Service/UrlValidationService.swift
+++ wallpaper-play/Service/UrlValidationService.swift
@@ -7,7 +7,7 @@ protocol UrlValidationService {
 
 class UrlValidationServiceImpl: UrlValidationService {
     func validate(string: String) -> URL? {
-        let urlRegEx = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$"
+        let urlRegEx = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.?[a-z]{2,6}(:[0-9]{1,5})?(/[-\\w@\\+\\.~#\\?&/=%]*)?$"
         let urlTest = NSPredicate(format:"SELF MATCHES %@", urlRegEx)
         let result = urlTest.evaluate(with: string)
         if result {
